39be6febbe29783d6905216c4ed5d7176711bdac
Santy <santi.codev@gmail.com>
2021-09-17 17:29:21 +0200

    Commit inicial

---
+++ src/main/java/com/santicodev/apicajerobanco/ApicajerobancoApplication.java
@@ -0,0 +1,13 @@
+package com.santicodev.apicajerobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicajerobancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicajerobancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santicodev/apicajerobanco/ApicajerobancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santicodev.apicajerobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicajerobancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
